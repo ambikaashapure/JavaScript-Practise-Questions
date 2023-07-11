938da127c6b4e9598972b303445840cf5e3205ca
Ambika Ashapure <119685403+ambikaashapure@users.noreply.github.com>
2023-07-11 12:05:19 +0530

    Update q4.js

--- q4.js
+++ q4.js
@@ -1,19 +1,22 @@
 //Solution to question 4
 
-let age = prompt("Enter you age");
 
-if(age < 18){
-    console.log("You're too young to be in here!");
-}
-else if( age >= 18 && age <=21){
-    console.log("Come on in but no drinking!");
-}
-else if(age >21 && age<=75){
-    console.log("Come on in!");
-}
-else if ( age>75){
-    console.log("Are you sure you want to be here?")
-}
-else{
-    console.log("Please enter a valid age");
+let age = prompt("Enter your age : ");
+
+switch (true) {
+    case age < 18:
+        console.log("You're too young to be in here!");
+        break;
+    case age >= 18 && age <= 21:
+        console.log("Come on in but no drinking!");
+        break;
+    case age > 21 && age <= 75:
+        console.log("Come on in!");
+        break;
+    case age > 75:
+        console.log("Are you sure you want to be here?");
+        break;
+    default:
+        console.log("Please enter a valid age");
+        break;
 }
